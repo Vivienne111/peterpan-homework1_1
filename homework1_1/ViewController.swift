c6e0bc05b94b4df8621e757aad7e1b71becf5f88
Vivienne <chchvivienne@gmail.com>
2020-11-13 17:38:13 +0800

    Initial Commit

---
+++ homework1_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework1_1
+//
+//  Created by Vivienne Chen on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
